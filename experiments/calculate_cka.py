d0b6c8fd9a4000957cbd6cb2aa854f4fea4fa039
Matt Gwilliam <mattgwilliamjr@gmail.com>
2021-10-31 18:09:21 -0400

    y

--- experiments/calculate_cka.py
+++ experiments/calculate_cka.py
@@ -46,7 +46,7 @@ def main():
     for layer_name, representation_a in features_a.items():
         representation_b = features_b[layer_name]
         if args.dataset == 'imagenet':
-            cur_CKA = cuda_cka.linear_CKA[::5](representation_a.cuda(), representation_b[::5].cuda())
+            cur_CKA = cuda_cka.linear_CKA(representation_a[::5].cuda(), representation_b[::5].cuda())
         else:
             cur_CKA = cuda_cka.linear_CKA(representation_a.cuda(), representation_b.cuda())
         print(f'Dataset: {args.dataset}, Backbone_A: {args.backbone_a}, Backbone_B: {args.backbone_b}, Layer: {layer_name}, Linear_CKA: {cur_CKA}', flush=True)
