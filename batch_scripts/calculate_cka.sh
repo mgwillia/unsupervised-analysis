0aa5aae4a328a748d128acb89c13acd48edd5225
Matt Gwilliam <mattgwilliamjr@gmail.com>
2021-10-31 18:07:28 -0400

    y

--- batch_scripts/calculate_cka.sh
+++ batch_scripts/calculate_cka.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=calc_cka
-#SBATCH --output=outfiles/calc_cka.out.%j
-#SBATCH --error=outfiles/calc_cka.out.%j
+#SBATCH --job-name=calc_cka_all
+#SBATCH --output=outfiles/calc_cka_all.out.%j
+#SBATCH --error=outfiles/calc_cka_all.out.%j
 #SBATCH --time=36:00:00
-#SBATCH --qos=high
+#SBATCH --qos=medium
 #SBATCH --gres=gpu:p6000:1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=32G
@@ -16,7 +16,9 @@ BACKBONES=("btwins_r50_1000" "dcv2_r50_800" "moco_r50_800" "simclr_r50_800" "sim
 
 srun bash -c "hostname;"
 for dataset in ${DATASETS[@]}; do
-    for backbone in ${BACKBONES[@]}; do
-        srun bash -c "python experiments/calculate_cka.py --backbone-a supervised_r50 --backbone-b $backbone --dataset $dataset;"
+    for backbone_a in ${BACKBONES[@]}; do
+        for backbone_b in ${BACKBONES[@]}; do
+            srun bash -c "python experiments/calculate_cka.py --backbone-a $backbone_a --backbone-b $backbone_b --dataset $dataset;"
+        done
     done
 done
