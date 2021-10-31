8e71780f75e1422ada564926b8321c568475ff16
Matt Gwilliam <mattgwilliamjr@gmail.com>
2021-10-31 16:46:36 -0400

    y

--- batch_scripts/calculate_cka_fgvc.sh
+++ batch_scripts/calculate_cka_fgvc.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#SBATCH --job-name=calc_cka
-#SBATCH --output=outfiles/calc_cka.out.%j
-#SBATCH --error=outfiles/calc_cka.out.%j
+#SBATCH --job-name=calc_cka_fgvc
+#SBATCH --output=outfiles/calc_cka_fgvc.out.%j
+#SBATCH --error=outfiles/calc_cka_fgvc.out.%j
 #SBATCH --time=36:00:00
 #SBATCH --qos=high
 #SBATCH --gres=gpu:gtx1080ti:1
@@ -11,7 +11,7 @@
 
 module load cuda/11.0.3
 
-DATASETS=("imagenet")
+DATASETS=("aircraft" "cars" "cub" "dogs" "flowers" "nabirds")
 BACKBONES=("btwins_r50_1000" "dcv2_r50_800" "moco_r50_800" "simclr_r50_800" "simsiam_r50_100" "swav_r50_800" "simclr_r50_100" "simclr_r50_200" "simclr_r50_400" "simclr_r50_1000")
 
 srun bash -c "hostname;"
